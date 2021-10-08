3baf7a7481518f2c6b6d2b126df6c520aaa3427f
gargmansi <gargmansi835@gmail.com>
2021-10-08 06:14:48 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Todolist  from './todolist';
+const App = () =>{
+  const [inputList, setInputList] = useState(" ");
+  const [Items, setItems ] = useState([]);
+  const itemEvent = (Event) =>
+  {
+    setInputList(Event.target.value);
+  };
+  const listOFItems  = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    
+    });
+    setInputList(" ");
+  };
+  return  (
+<>
+<div className = "main_div">
+  <div className = "center_div">
+    <br/>
+    <h1>ToDo list</h1>   <input type = "text" placeholder="Add a items"
+    value={inputList}
+    onChange={itemEvent} / >
+    <button onClick={listOFItems}> + </button>
+    <ol> 
+ { /*<li> {inputList} </li> */ }
+ {Items.map((itemval) =>{
+  return <li> {itemval} </li>;
+ }
+  )}
+    </ol>
+  </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+</div>
 
+
+
+</>
+)
+};
 export default App;
+ 
---
+++ src/todolist.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Todolist = (props)  => {
+    return <li> 
+        {props.todolist}
+    </li>
+};
+export default Todolist;
